6fc10c209e5e8211ec080f01f1e4977fb16c1ddd
Bruna Rossa <122941936+brunarossa@users.noreply.github.com>
2024-04-04 11:06:08 +0100

    Nível 4

---
+++ Van.java
@@ -0,0 +1,22 @@
+public class Van extends GroundTransportation {
+    private int packages;
+
+    public Van(String licensePlate, int packages) {
+        super(licensePlate);
+        this.packages = packages;
+    }
+
+    public int getPackages() {
+        return packages;
+    }
+
+    public void setPackages(int packages) {
+        this.packages = packages;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                String.format("%15s: %d\n", "Número de Pacotes", packages);
+    }
+}
