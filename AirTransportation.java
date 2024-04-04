5ca330806b390871ecafba461e6157d781d7f6bb
Bruna Rossa <122941936+brunarossa@users.noreply.github.com>
2024-04-04 10:53:36 +0100

    Nível 2 e 3

--- AirTransportation.java
+++ AirTransportation.java
@@ -24,4 +24,34 @@ public class AirTransportation extends Transport {
     public void setNumberOfContainers(int numberOfContainers) {
         this.numberOfContainers = numberOfContainers;
     }
+
+    public double getPriceWithFees() {
+        return 0.0;
+    }
+
+    public String getTransportType() {
+        return "Transporte Aereo";
+    }
+
+    @Override
+    public double getFees() {
+        return 4.0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));
+        sb.append(String.format("%15s: %s\n", "ID", getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+        sb.append(String.format("%15s: %s\n", "Nome", this.name));
+        sb.append(String.format("%15s: %s\n", "NºContentores", this.numberOfContainers));
+
+        return sb.toString();
+    }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -14,4 +14,29 @@ public class GroundTransportation extends Transport {
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
     }
+
+    public String getTransportType() {
+        return "Transporte Terrestre";
+    }
+
+    @Override
+    public double getFees() {
+        return 3.0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));
+        sb.append(String.format("%15s: %s\n", "ID", getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+        sb.append(String.format("%15s: %s\n", "Matricula", this.licensePlate));
+
+        return sb.toString();
+    }
 }
